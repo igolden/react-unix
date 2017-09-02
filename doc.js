12f405f89ea4a9a6d7749fa87468d694e4114e03
Ian Golden <ian@iangolden.com>
2017-09-02 03:54:05 -0600

    initial

---
+++ doc.js
@@ -0,0 +1,37 @@
+/*
+  Hello World class
+
+  - Hello
+
+*/
+
+
+/**
+* This is the description for my class.
+*
+* @class MyClass
+* @constructor
+*/
+
+/**
+  My method description.  Like other pieces of your comment blocks, 
+  this can span multiple lines.
+ 
+  @method methodName
+  @param {String} foo Argument 1
+  @param {Object} config A config object
+  @param {String} config.name The name on the config object
+  @param {Function} config.callback A callback function on the config object
+  @param {Boolean} [extra=false] Do extra, optional work
+  @return {Boolean} Returns true on success
+*/
+
+/**
+* My property description.  Like other pieces of your comment blocks, 
+* this can span multiple lines.
+* 
+* @property propertyName
+* @type {Object}
+* @default "foo"
+*/
+
---
+++ index.js
@@ -0,0 +1,5 @@
+const libs = require('./lib');
+
+let terminal = new libs.Terminal({})
+
+terminal.vimTest()
